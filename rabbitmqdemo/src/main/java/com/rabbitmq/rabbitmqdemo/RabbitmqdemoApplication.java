45f6deb128a91aaf973b15ef19949da56cb4bc40
Márcio Araújo <maritsu2015@gmail.com>
2019-09-20 18:45:45 -0300

    migration to github

---
+++ rabbitmqdemo/src/main/java/com/rabbitmq/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -0,0 +1,28 @@
+package com.rabbitmq.rabbitmqdemo;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqdemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqdemoApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        SimpleMessage sm = new SimpleMessage();
+
+        sm.setName("yololo message");
+        sm.setDescription("simpleDescription");
+        rabbitTemplate.convertAndSend("MyTopicExchange", "topic", sm);
+    }
+}
